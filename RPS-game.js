c9c785589c715d4d569a5263445ec2626274b0fc
Sean Bonus <snxbns@gmail.com>
2022-07-14 17:02:41 -0400

    added playRound function

--- RPS-game.js
+++ RPS-game.js
@@ -1,4 +1,6 @@
 let randomRPS;
+let playerSelection;
+let computerSelection;
 
 function computerPlay() {
     const RPS = ['rock', 'paper', 'scissors'];
@@ -6,3 +8,24 @@ function computerPlay() {
     return RPS[randomRPS];
 }
 
+function playRound(playerSelection, computerSelection) {
+    let winner;
+
+    if (playerSelection === computerSelection) {
+        winner = "The game is a tie!";
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        winner = "Computer wins! Paper beats rock!";
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        winner = "User wins! Rock beats scissors!";
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        winner = "User wins! Paper beats rock!";
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        winner = "Computer wins! Scissors beats paper!";
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        winner = "User wins! Scissors beats paper!";
+    } else {
+        winner = "Computer wins! Rock beats scissors!";
+    }
+
+    return winner;
+}
